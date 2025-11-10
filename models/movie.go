8dad22e0b1321beaedb35189798162a893b44f65
AlekseiVeretennikov <149715005+AlekseiVeretennikov@users.noreply.github.com>
2025-11-10 21:49:25 +0300

    Delete models directory

--- models/movie.go
+++
@@ -1,9 +0,0 @@
-// file: sessions-service/internal/models/movie.go
-package models
-
-type Movie struct {
-	ID              int    `json:"id"`
-	Title           string `json:"title"`
-	Description     string `json:"description"`
-	DurationMinutes int    `json:"duration_minutes"`
-}
